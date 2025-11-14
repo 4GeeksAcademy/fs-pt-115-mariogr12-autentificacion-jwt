5904f434abc312dce324237e77f8457c1ee3c225
mariogr12 <mariogrguitar12@gmail.com>
2025-11-14 16:10:28 +0000

    hash, login y register hecho

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=["POST"])
+def signup():
+    data = request.get_json()
+
+    if not data["email"] or not data["password"]:
+        return jsonify({"msg": "email and password are required"}), 400
+    
+    existing_user = db.session.execute(db.select(User).where(
+        User.email == data["email"]
+    )).scalar_one_or_none()
+
+    if existing_user:
+        return jsonify({"msg": "an user already exists with this email"})
+    
+    new_user = User(email = data["email"])
+    new_user.set_password(data["password"])
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created successfully"})
+
+@api.route("/login",methods=["POST"])
+def login():
+    data = request.get_json()
+
+    if not data["email"] or not data["password"]:
+        return jsonify({"msg": "email and password are required"}), 400
+    
+    user = db.session.execute(db.select(User).where(
+        User.email == data["email"]
+    )).scalar_one_or_none()
+
+    if user is None:
+        return jsonify({"msg": "invalid email or password"}), 401
+    
+    if user.check_password(data["password"]):
+        return jsonify({"msg": "login successful"}), 200
+    else:
+        return jsonify({"msg": "invalid email or password"}), 401
+
